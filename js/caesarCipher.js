b00af957e31056218fed216432fc44a63d0b134b
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-01 18:01:31 -0500

    js solution for caesarCipher

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,16 @@
-exports.caesarCipher = function() {
-
+exports.caesarCipher = function (str, shiftAmount) {
+  let result = "";
+  for (let char of str) {
+    if (char.toLowerCase().match(/[a-z]/)) {
+      let num = char.charCodeAt(0) + shiftAmount;
+      if (shiftAmount > 0 && ((num > 90 && num < 97) || num > 122)) {
+        num -= 26;
+      } else if (shiftAmount < 0 && (num < 65 || (num < 97 && num > 90))) {
+        num += 26;
+      }
+      char = String.fromCharCode(num);
+    }
+    result += char;
+  }
+  return result;
 };
