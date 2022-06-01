e5d943ecb84a6a090d83a35e2bac427001c5e33f
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-01 16:51:36 -0500

    python solution for caesar_cipher

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,12 @@
 def caesar_cipher(string, shift_amount):
+    result = ""
+    for char in string:
+        if char.isalpha():
+            num = ord(char) + shift_amount
+            if ((shift_amount > 0) and ((num > 90 and num < 97) or (num > 122))):
+                num -= 26
+            elif((shift_amount < 0) and ((num < 65) or (num < 97 and num > 90))):
+                num += 26
+            char = chr(num)
+        result += char
+    return result
